b75368501d7bbe20c1c30f9ee539989254a5e2b3
Dan Tasse <dan.tasse@gmail.com>
2016-10-05 19:53:13 +0000

    oops I don't know how to bash

--- make_all_yfcc100m_city_dumps.sh
+++ make_all_yfcc100m_city_dumps.sh
@@ -3,8 +3,8 @@
 for CITY in pgh sf ny houston detroit chicago cleveland seattle miami london minneapolis austin sanantonio dallas
 do
     echo "Starting city: $CITY"
-    ./flickr_in_city.py --yfcc_file=../yfcc100m_dataset --city=$CITY --output_file=temp_yfcc100m_$CITY.csv
-    ./add_autotags.py --city_csv_file=temp_yfcc100m_$CITY.csv --autotags_file=../yfcc100m_autotags --output_file=yfcc100m_$CITY_autotagged.csv
-    rm temp_yfcc100m_$CITY.csv
+    ./flickr_in_city.py --yfcc_file=../yfcc100m_dataset --city=${CITY} --output_file=temp_yfcc100m_${CITY}.csv
+    ./add_autotags.py --city_csv_file=temp_yfcc100m_${CITY}.csv --autotags_file=../yfcc100m_autotags --output_file=yfcc100m_${CITY}_autotagged.csv
+    rm temp_yfcc100m_${CITY}.csv
     echo "Done with $CITY"
 done
